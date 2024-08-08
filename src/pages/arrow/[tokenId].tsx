d02513d97d4eebfbe205202e726677fe9a848dd1
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-08-09 01:31:30 +0530

    Update meta tags for Farcaster integration in arrow page

--- src/pages/arrow/[tokenId].tsx
+++ src/pages/arrow/[tokenId].tsx
@@ -36,7 +36,7 @@ const Home: NextPage<{ token?: Nft, farcasterAccount?: any, ownerAddress?: any }
         <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={token.image.thumbnailUrl} />
+        <meta property="fc:frame:image" content={token.image.originalUrl} />
         <meta property="og:image" content={token.image.thumbnailUrl} />
         <meta property="fc:frame:image:aspect_ratio" content="1:1" />
         <meta property="fc:frame:button:1" content="Check this out" />
