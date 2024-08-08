820d9c3c301cf859d9de75f3117ea231bd94fedd
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-08-09 01:24:41 +0530

    chore: Disable "@typescript-eslint/no-unsafe-return" rule in ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ const config = {
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
