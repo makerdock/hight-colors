5c11f60a40758f134462740454c703c6c75fac03
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-08-09 17:26:52 +0530

    refactor: Improve Nav component UI and add logout button

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,6 +3,9 @@ import {
     useConnectModal
 } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
+import classnames from "classnames"
+import { LogOutIcon } from "lucide-react";
+import { Button } from "./ui/button";
 
 const Nav = () => {
     const { openConnectModal } = useConnectModal();
@@ -16,20 +19,29 @@ const Nav = () => {
     const buttonClasses = "px-4 py-2 bg-white text-black font-medium rounded-full hover:bg-black hover:text-white transition-colors duration-200";
 
     return (
-        <nav className="flex justify-center py-6 fixed top-0 left-0 w-full z-30">
-            <div className="backdrop-blur-md bg-white/30 rounded-full shadow-lg">
-                {!address && (
-                    <button onClick={openConnectModal} className={buttonClasses}>
-                        Connect Wallet
-                    </button>
-                )}
-                {!!address && (
-                    <button onClick={openAccountModal} className={buttonClasses}>
-                        {walletFormat(address)}
-                    </button>
-                )}
-            </div>
-        </nav>
+        <>
+            <nav className="flex justify-center py-6 fixed top-0 left-0 w-full z-30">
+                <div className="backdrop-blur-md bg-white/30 rounded-full shadow-lg">
+                    {!address && (
+                        <button onClick={openConnectModal} className={buttonClasses}>
+                            Connect Wallet
+                        </button>
+                    )}
+                    {!!address && (
+                        <button onClick={openAccountModal} className={classnames(buttonClasses, "invisible sm:visible")}>
+                            {walletFormat(address)}
+                        </button>
+                    )}
+
+
+                </div>
+            </nav>
+            {!!address && (
+                <Button variant={"ghost"} onClick={openAccountModal} className={classnames("md:invisible visible absolute top-2 right-2 ml-auto z-50 shadow-lg border border-gray-200 text-gray-600 rounded-full cursor-pointer")}>
+                    <LogOutIcon className="h-4 w-4" />
+                </Button>
+            )}
+        </>
     );
 };
 
